27d148172adf3a95c6d250338d5d5050c82b8515
Carlos V <cavarod@gmail.com>
2021-02-22 13:11:05 -0800

    Added button behavior to create a new task on click or on form submission.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+
+// Get information from the DOM
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+
+	// Prevents the browser from reloading
+	event.preventDefault();
+
+	// Create the next li element in the DOM
 	var listItemEl = document.createElement("li");
 	listItemEl.className = "task-item";
 	listItemEl.textContent = "This is a new task.";
 	tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
